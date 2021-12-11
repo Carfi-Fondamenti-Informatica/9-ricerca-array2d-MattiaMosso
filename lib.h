87f0f8f976050b8248fb62c88ab081d7040638db
MattiaMosso <91486653+MattiaMosso@users.noreply.github.com>
2021-12-11 10:17:16 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
+#ifndef GITHUB_COMPITO9CORRETTO_LIB_H
+#define GITHUB_COMPITO9CORRETTO_LIB_H
 
+bool ricerca(char lista [10][20], char nome [1][20], int &pos);
+
+#endif //GITHUB_COMPITO9CORRETTO_LIB_H
